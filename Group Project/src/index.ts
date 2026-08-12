fb5ddcbef6a46ed5dea352174856914419a49613
Devaughn-21 <Dchanner3201@conestogac.on.ca>
2026-08-12 13:05:22 -0400

    Updated packages, added cors to allow frontend implementation

--- Group Project/src/index.ts
+++ Group Project/src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import cors from "cors";
 import carRoutes from "./routes/cars";
 import userRoutes from "./routes/users";
 import paymentRoutes from "./routes/payments";
 import rentalRoutes from "./routes/rentals";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (_req, res) => {
